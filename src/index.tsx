5ec32578aaa3d194f5f6744a6871c6bb9d00090b
ArtemAlagizov <alagizov@gmail.com>
2020-02-15 13:09:39 +0100

    added /particalizor-3000-showcase basename

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./components/app/container/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router basename="/particalizor-3000-showcase">
+    <Switch>
+      <Route exact path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
